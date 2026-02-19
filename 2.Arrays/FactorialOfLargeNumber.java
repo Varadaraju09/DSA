f56f8a27e19c3886b17a70a335797f9dfc8806a4
Varadaraju T A <varadaraju092005@gmail.com>
2026-02-19 16:38:52 +0530

    Fix variable initialization in factorial calculation

--- 2.Arrays/FactorialOfLargeNumber.java
+++ 2.Arrays/FactorialOfLargeNumber.java
@@ -1,7 +1,7 @@
 public class FactorialOfLargeNumber {
     public static int fact(int n) {
-        int n1;
-        int n2 
+        int n1 = 1;
+        int n2 = 1;
         int res;
         while(n > 0) {
             res = n1 * n2;
