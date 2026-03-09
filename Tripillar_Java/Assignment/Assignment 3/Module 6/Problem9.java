7c149854993bb86486f2cfc9bc11e9ea8476bfbb
Varadaraju09 <varadaraju092005@gamil.com>
2026-03-09 21:46:27 +0530

    Assignment 3 Updated

--- Tripillar_Java/Assignment/Assignment 3/Module 6/Problem9.java
+++ Tripillar_Java/Assignment/Assignment 3/Module 6/Problem9.java
@@ -1,5 +1,6 @@
-public class MissingNumber {
+// 9. Missing Number (1 to n)
 
+public class Problem9 {
     public static int missingNumber(int[] arr) {
         int n = arr.length + 1;
         int totalSum = (n * (n + 1)) / 2;
